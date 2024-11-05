7ba5bb7cf008a4074c7f2d2bc6bfbcddf2987334
michaellgans <michaellgans@Victoria>
2024-11-05 11:16:02 -0600

    MLG - 11:15 AM - Creates basic layout for header before styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 // Header Component
 
 // Asset Imports
+import { FilmIcon } from "./FilmIcon"
+import { LogoutIcon } from "./LogoutIcon";
 
 // Script Imports
 
@@ -13,8 +15,18 @@
 export function Header() {
     // Define Hook
     return (
-        <div>
-            This is a test!
+        <div className="flex justify-between">
+            {/* TITLE AND ICON */}
+            <div className="flex">
+                <FilmIcon />
+                <h1>Cinema Guru</h1>
+            </div>
+            {/* WELCOME AND LOGOUT */}
+            <div className="flex">
+                <h3>Welcome, email.name@hotmail.com!</h3>
+                <LogoutIcon />
+                <p>Logout</p>
+            </div>
         </div>
     );
 }
