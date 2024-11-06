567b8933c21ff54cab30e1ee391b585ab5cb1770
michaellgans <michaellgans@Victoria>
2024-11-06 00:43:48 -0600

    MLG - 12:43 PM - styles buttons

--- components/GenreButton.tsx
+++ components/GenreButton.tsx
@@ -13,7 +13,7 @@ type GenreButtonProps = {
 export function GenreButton({ genre }: GenreButtonProps) {
   // Define Hook
   return (
-    <button className="h-9 rounded-full border-2 border-lumi-teal px-3">
+    <button className="h-9 rounded-full border-2 border-lumi-teal px-3 hover:bg-lumi-teal hover:text-lumi-navy active:bg-lumi-dark-teal">
       {genre}
     </button>
   );
