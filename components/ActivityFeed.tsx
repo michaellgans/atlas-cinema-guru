7208e75c14f7a5b33a4b28b3fd785a06cd2faa8b
michaellgans <michaellgans@Victoria>
2024-11-05 23:10:06 -0600

    Creates ActivityItem Components

--- components/ActivityFeed.tsx
+++ components/ActivityFeed.tsx
@@ -1,6 +1,7 @@
 // ActivityFeed Component
 
 // Asset Imports
+import { ActivityItem } from "./ActivityItem";
 
 // Script Imports
 
@@ -14,8 +15,21 @@
 export function ActivityFeed() {
   // Define Hook
   return (
-    <div className="flex w-52 justify-center rounded-lg bg-lumi-teal">
-      <h2 className="px-7 py-5 text-xl font-bold">Latest Activity</h2>
+    <div className="mb-7 flex w-52 flex-col justify-center rounded-lg bg-lumi-teal">
+      <h2 className="px-6 py-4 text-center text-xl font-bold">
+        Latest Activity
+      </h2>
+      <div className="px-2">
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+        <ActivityItem />
+      </div>
     </div>
   );
 }
---
+++ components/ActivityItem.tsx
@@ -0,0 +1,24 @@
+// ActivityItem Component
+
+// Asset Imports
+
+// Script Imports
+
+// Types
+// type ActivityItemProps = {
+//     prop: type;
+//     prop: type;
+// };
+
+// Returns a ActivityItem Component
+export function ActivityItem() {
+  // Define Hook
+  return (
+    <div className="mb-2 font-medium">
+      <p>10/2/2024, 5:11:17 PM</p>
+      <p>
+        Added <span className="font-bold">Before the Dawn</span> to watch later
+      </p>
+    </div>
+  );
+}
