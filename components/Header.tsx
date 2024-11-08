a06e3d4a7a3154043ec726fd923943f02d734863
michaellgans <michaellgans@Victoria>
2024-11-07 22:21:49 -0600

    MLG - 10:21 PM - enables logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,18 @@
 // Header Component
+"use client";
 
 // Asset Imports
 import { FilmIcon } from "./FilmIcon";
 import { LogoutIcon } from "./LogoutIcon";
-
-// Script Imports
-
-// Types
-// type HeaderProps = {
-//     children: React.ReactNode;
-// };
+import { signOut } from "next-auth/react";
 
 // Returns a Header Component
 export function Header() {
   // Define Hook
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <div className="text-large flex h-16 items-center justify-between bg-lumi-teal px-4 font-poppins text-lumi-navy">
       {/* TITLE AND ICON */}
@@ -24,8 +23,13 @@ export function Header() {
       {/* WELCOME AND LOGOUT */}
       <div className="flex font-medium">
         <h3 className="pr-3">Welcome, email.name@hotmail.com!</h3>
-        <LogoutIcon />
-        <p className="pl-1">Logout</p>
+        <button
+          onClick={handleLogout}
+          className="flex items-center hover:text-white"
+        >
+          <LogoutIcon />
+          <p className="pl-1">Logout</p>
+        </button>
       </div>
     </div>
   );
