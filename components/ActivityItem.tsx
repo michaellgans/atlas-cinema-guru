ff6752c76449c313a89293204629fa62e25283d1
michaellgans <michaellgans@Victoria>
2024-11-07 19:28:48 -0600

    MLG - 7:28 PM - updates ActivityItem to get current date and time

--- components/ActivityItem.tsx
+++ components/ActivityItem.tsx
@@ -1,23 +1,43 @@
 // ActivityItem Component
+"use client";
 
 // Asset Imports
-
-// Script Imports
+import { useState, useEffect } from "react";
 
 // Types
-// type ActivityItemProps = {
-//     prop: type;
-//     prop: type;
-// };
+type ActivityItemProps = {
+  id: string;
+  title: string;
+  collection: string;
+};
 
 // Returns a ActivityItem Component
-export function ActivityItem() {
+export function ActivityItem({ id, title }: ActivityItemProps) {
   // Define Hook
+  const [currentDate, setCurrentDate] = useState<string>("");
+  const [currentTime, setCurrentTime] = useState<string>("");
+
+  useEffect(() => {
+    const today = new Date();
+    const timeStamp = new Intl.DateTimeFormat("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    }).format(today);
+    const formatDate = today.toLocaleDateString();
+
+    setCurrentDate(formatDate);
+    setCurrentTime(timeStamp);
+  }, []);
+
   return (
     <div className="mb-2 font-medium">
-      <p>10/2/2024, 5:11:17 PM</p>
       <p>
-        Added <span className="font-bold">Before the Dawn</span> to watch later
+        {currentDate}, {currentTime}
+      </p>
+      <p>
+        Added <span className="font-bold">{title}</span> to watch later
       </p>
     </div>
   );
